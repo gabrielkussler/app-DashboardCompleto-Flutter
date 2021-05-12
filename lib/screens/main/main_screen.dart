6e6e926f8d356e874020e91e6a35674397d36080
gabrielkussler <gkgabrielkussler@gmail.com>
2021-05-12 17:56:01 -0300

    logo.png no canto superior esquerdo

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -7,7 +7,25 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-
+        child: Row(
+          children: [
+            Expanded(
+              //default flex = 1/6 (proporçao na tela)
+                child: Column(
+                  children: [
+                    DrawerHeader(child: Image.asset("assets/images/logo.png"))
+                  ],
+                ),
+            ),
+            Expanded(
+              //default flex = 5/6 (proporçao na tela)
+            flex: 5,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
